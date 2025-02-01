e6cc6fdf1f4d71a79691b51d4a9d109cc1e0461b
loser <34392022+loserc6@users.noreply.github.com>
2025-02-01 18:03:17 +0800

    Update diy6.0.sh

--- diy6.0.sh
+++ diy6.0.sh
@@ -45,7 +45,7 @@ merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclas
 git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
 # git clone https://github.com/sbwml/openwrt-alist.git package/openwrt-alist
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
-git clone https://github.com/fw876/helloworld.git package/ssr
+#git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
@@ -56,13 +56,13 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+# git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 # git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 # git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5-lua package/mosdns
-git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+# git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 # git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
 # git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
 # git clone https://github.com/BCYDTZ/luci-app-UUGameAcc.git package/luci-app-UUGameAcc
